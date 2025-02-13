d560825e3eb00ce9643b3a7d9989c3a93f49b183
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2025-02-13 23:30:04 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(): React.ReactElement {
+  return (
+    <div className={'w-full h-screen font-display bg-gray-200'}>
+      <p className={'text-gray-800'}>
+        Whereas disregard and contempt for human rights have resulted
+      </p>
+    </div>
+  );
+}
+
+export default App;

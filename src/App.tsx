94622734231f16c14297c26a89ea22bce3506b81
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2025-02-20 23:59:21 +0530

    Install DaisyUI Beta Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Navbar } from './components';
 
 function App(): React.ReactElement {
   return (
-    <div className={'w-full h-screen font-display bg-gray-200'}>
-      <Navbar />
+    <div className={'flex flex-col text-red-500'}>
+      <div>{'Hello World!'}</div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </div>
   );
 }

3ae6c58482a83b1c6ebcde53ed76dea10dfef3b0
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2025-02-25 20:40:13 +0530

    Add Sign up page, AuthImagePattern component

---
+++ src/components/auth-image-pattern/index.ts
@@ -0,0 +1,1 @@
+export { default as AuthImagePattern } from './auth-image-pattern.tsx';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Navbar } from './navbar';
 export { ProtectedRoute } from './protected-route';
 export { Loading } from './loading';
+export { AuthImagePattern } from './auth-image-pattern';
